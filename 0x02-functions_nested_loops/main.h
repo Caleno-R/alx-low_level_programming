7fbfb422434b8ba9e2fa3ec3b9a96868c638c719
Caleno-R <calenorose@gmail.com>
2022-03-15 18:25:58 +0300

    the header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Rose Caleno
+ * Description: This header file contains declarations for all tasks in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
